75ac4aa14079e8bee13a18765035604bec99d9da
Cornelius Soon <cornelius.soon@gmail.com>
2023-11-24 16:14:00 +1300

    Initialise project

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
+import "./stylesheets/minimum.css";
+import "./stylesheets/style.css";
+
 (() => {
+    const menuWindow = document.createElement("nav");
+    menuWindow.classList.add("menu-window");
+    menuWindow.classList.add("hidden");
+
+    const menuButton = document.createElement("button");
+    menuButton.textContent = "Menu";
+    menuButton.addEventListener("touchstart", () => {
+        menuWindow.classList.add("visible");
+    });
+
+    const sequence = ["My Profile", "Messages", "Saved Posts", "Settings"];
+
+    function createMenuElement(title) {
+        const menuElement = document.createElement("div");
+        menuElement.textContent = title;
+        menuWindow.appendChild(menuElement);
+        return menuElement;
+    }
+
+    function buildMenuElement(title) {
+        return createMenuElement(title);
+    }
+
+    menuWindow.appendChild(buildMenuElement("My Profile"));
+    document.body.appendChild(menuWindow);
+    document.body.appendChild(menuButton);
 
     return {};
 })();
